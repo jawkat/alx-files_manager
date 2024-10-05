f28e06ca15bf3b2517d32101ab2864dc0aa18f2d
Abdelrahman <boda8099@gmail.com>
2024-10-05 03:48:17 +0300

    new

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const express = require('express');
 const api = express.Router();
 const AuthController = require('../controllers/AuthController');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 api.get('/status', AppController.getStatus);
 api.get('/stats', AppController.getStats);
 
+api.get('/users', UsersController.postNew);
+
 api.get('/connect', AuthController.getConnect);
 api.get('/disconnect', AuthController.getDisconnect);
+api.get('/users/me', UsersController.getMe);
 module.exports = api;
